fbad419deeafbf74c924e9581cf5469d74d2194e
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-06-10 23:44:58 +0530

    Added Local server

---
+++ src/local.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request
+
+__author__ = '0xprateek'
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+    return '',204
+
+if __name__ == "__main__":
+    app.run(port=5050, debug=True)
