f68e90b003dd8fe382e05aa8c6bebaad72814313
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-07-04 23:49:14 +0530

    Fixed - Local server outputs

--- src/dash.py
+++ src/dash.py
@@ -1,70 +1,51 @@
-import random,sys
-import time,subprocess
+from subprocess import Popen,PIPE
+import threading
+import os
 
-def header():
-    print("\n\t\tDaSh")
-    print("\tFuckin logo lies here...!\n")
+def process_display(type,message):
 
-def display(status,message,verbose):
-    if verbose:
-        if status == 0:
-            print(message)  # For success green color
-        else:
-            print(message) # For failure red color
+    if type == 0:
+        print("[OK] ~",message)
+    elif type == 1:
+        print("[NO] ~",message)
+    elif type == 2:
+        print("[!] ~",message)
 
-def process_local(output):
-    if "Running on" in output:
-        address = output[15:37]
-        print("[1/2] Local server is ready...")
-    else:
-        print("~~> ",output)
-    '''
-    elif '"GET / HTTP/1.1" 200 ' in output:
-        print("[~] Someone opened your Uploader... :)")
 
-    else:
-        http_code = output[-5:-2]
-        if http_code == 204:
-            print("\n------- Congrats ------")
-            print("File uploaded successfully :< ")
-    '''
+def local():
 
-def local(port,path):
-    local_process = subprocess.Popen(['python3 ./local.py'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell = True)
-    for line in iter(local_process.stdout.readline, b''):
-        process_local(str(line.decode('utf-8')))
+    local_process = Popen(['python3 ./local.py'],stderr = PIPE,stdout = PIPE,shell = True)
+    for output in local_process.stderr:
+        output=output.decode('utf-8')
+        if "Traceback" in output:
+            process_display(1,"Error due to usable port.")
 
-# packet_write_wait: Connection to 159.89.214.31 port 22: Broken pipe
+        elif "Running" in output:
+            process_display(0,"[1/2] Local Server is started.")
+
+        elif "127.0.0.1" in output:
+            http_code = int(output[-6:-2])
+            if http_code == 200:
+                process_display(0,"Someone Opened the page.")
+            elif http_code == 204:
+                process_display(0,"Upload Completed.")
 
 def forward():
-    serveo = subprocess.Popen(['ssh','-R','80:localhost:5050','serveo.net'],stdout=subprocess.PIPE)
-    line = serveo.stdout.readline()
-    print(str(line.decode('utf-8')),end="")
+    forward_process = Popen(['ssh -R 80:localhost:5050 serveo.net'],stdout = PIPE, stderr = PIPE,shell = True)
+    print(forward_process.stderr.readline())
+    print("Koi scene hai kya  .. :/")
+    for output in forward_process.stdout:
+        print(output.decode('utf-8'))
 
 if __name__ == "__main__":
+    print("\t\t  DASh \n\t Fucking logo lies here\n")
 
-    # Prints logo on console.
-    header()
 
-    try:
-        import sys,argparse
-        parser = argparse.ArgumentParser()
-    except:
-        print("Moudle not found")
-
-    parser.add_argument('-port',help="Port address",required = False,default = 5050)
-    parser.add_argument('-path',help="Path to save file",required = False,default='/home/usr/Desktop/My')
-    parser.add_argument('-v',help="Verbose",default=False, action='store_true',required = False)
-
-    args = parser.parse_args()
-    verbose = args.v
-
-    import threading
-
-    thread1 = threading.Thread(target=local,args=(args.port,args.path))
+    thread1 = threading.Thread(target=local)
     thread2 = threading.Thread(target=forward)
 
     thread1.start()
     thread2.start()
+
     thread1.join()
     thread2.join()
