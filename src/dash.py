069d20be0fe814b529e05a39ccc13646e0e4be8f
Prateek Mishra <pr4t333k@gmail.com>
2019-07-14 13:19:05 +0530

    Adding No verbose feature - Issue Fix #9

--- src/dash.py
+++ src/dash.py
@@ -5,13 +5,17 @@ def header():
     print("\tFuckin logo lies here...!\n")
 
 
-def process_display(type,message):
-    if type == 0:
-        print("[OK] ~",message)
-    elif type == 1:
-        print("[NO] ~",message)
-    elif type == 2:
-        print("[!] ~",message)
+def process_display(verbose,type,message):
+
+    if args.v is False:
+        verbose = 0
+
+    if type == 0 and verbose == 0:
+        print("[+]",message)
+    elif type == 1 and verbose == 0:
+        print("[-]",message)
+    elif type == 2 and verbose == 0:
+        print("[~]",message)
 
 def local(port,path):
 
@@ -20,37 +24,37 @@ def local(port,path):
     for output in local_process.stdout:
         output=output.decode('utf-8')
         if "Traceback" in output:
-            process_display(1,"Error due to usable port.")
+            process_display(0,1,"Error due to usable port.")
         elif "Running" in output:
-            process_display(0,"[1/2] Local Server is started.")
+            process_display(1,0,"[1/2] Local Server is started.")
         elif "ClientIP" in output:
             ClientIP = output[10:]
         elif "127.0.0.1" in output:
             out = str(output).split(' ')
             if out[6] == '/':
-                process_display(0,"Someone Opened the page at ")
+                process_display(0,0,"Someone Opened the page")
             elif out[-2] == '204':
-                process_display(0,"Upload Completed by ")
+                process_display(0,0,"Upload Completed")
 
 def forward(port):
     import requests
     import json
 
     ngrok = subprocess.Popen(['ngrok','http',str(port)],stdout=subprocess.PIPE)
-    process_display(0,"[2/2] Public Server is started.")
+    process_display(1,0,"[2/2] Public Server is started.")
     time.sleep(3)
     tunnel_url = requests.get("http://localhost:4040/api/tunnels").text
     j = json.loads(tunnel_url)
     try:
         tunnel_url = j['tunnels'][0]['public_url']
-        process_display(0,"The link for the page is : "+tunnel_url)
+        process_display(0,0,"The link for the page is : "+tunnel_url)
     except IndexError:
-        process_display(2,"Rechecking the URLs in 4s :/ ")
+        process_display(1,2,"Rechecking the URLs in 4s :/ ")
         time.sleep(4)
         tunnel_url = requests.get("http://localhost:4040/api/tunnels").text
         j = json.loads(tunnel_url)
         tunnel_url = j['tunnels'][0]['public_url']
-        process_display(0,"The link for the page is : "+tunnel_url)
+        process_display(0,0,"The link for the page is : "+tunnel_url)
 
     print("\n------------------------------- _^_ -------------------------------\n")
 
@@ -64,10 +68,11 @@ if __name__ == "__main__":
         import sys,argparse
         parser = argparse.ArgumentParser()
     except:
-        print("Moudle not found")
+        process_display(1,1,"Moudle not found")
 
     parser.add_argument('-port',help="Port address",required = False,default = 5050)
     parser.add_argument('-path',help="Path to save file",required = False,default=os.path.expanduser('~')+'/Desktop')
+    parser.add_argument('-v',help="Verbose",default=False, action='store_true',required = False)
 
     args = parser.parse_args()
 
