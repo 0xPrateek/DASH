a364ee44aacfbff4785dba3332ff84e29f9d2acd
Prateek Mishra <pr4t333k@gmail.com>
2019-10-05 21:19:46 +0530

    Logo.py implementation of logo.png

---
+++ Logo/logo.py
@@ -0,0 +1,22 @@
+def logo():
+    black = '\033[30m'
+    red = '\033[31m'
+
+    print(r'''
+{}*****************************************************
+**{}********{}*******{}****{}******{}**********{}**{}****{}****{}****{}**
+**{}*{}        {}*{}****{}*{}    {}*{}*****{}*{}       {}**{}**{}*{}  {}*{}****{}*{}  {}*{}**
+**{}*{}  {}*{}***{}*{}  {}*{}**{}*{}  {}*{}{}*{}  {}*{}****{}*{}  {}*{}********{}*{}  {}*{}****{}*{}  {}*{}**
+**{}*{}  {}*{}***{}*{}  {}*{}*{}*{}        {}*{}***{}*{}        {}*{}**{}*{}    {}*{}{}*{}    {}*{}**
+**{}*{}  {}*{}***{}*{}  {}*{}{}*{}  {}*{}****{}*{}  {}*{}********{}*{}  {}*{}**{}*{}  {}*{}****{}*{}  {}*{}**
+**{}*{}        {}*{}{}*{}  {}*{}******{}*{}  {}*{}*{}**{}       {}*{}**{}*{}  {}*{}****{}*{}  {}*{}**
+**{}**************{}*******{}**************{}**{}****{}****{}****{}**
+*****************************************************
+    '''.format(red,
+               black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red,black,red,red,red,red,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,black,red,
+               black,red,black,red,black,red,black,red))
