ea613578a455a8a2451d7139e041f2eaa3def4cb
Prateek Mishra <pr4t333k@gmail.com>
2019-10-09 00:03:05 +0530

    Handling exceptions in DASH.py

--- src/dash.py
+++ src/dash.py
@@ -1,4 +1,10 @@
-import time,subprocess,os,colors
+import sys
+
+try:
+    import time,subprocess,os,colors
+except:
+    process_display(1,1,"Module not found\n    Make sure you have installed 'requirements.txt' and configured DASH")
+    sys.exit(0)
 
 def header():
     print('\n\t\t{}-{} DASH {}-{}\n'.format(colors.red,colors.white,colors.red,colors.white))
@@ -40,11 +46,16 @@ def local(port,path):
                 process_display(0,0,"Upload Completed")
 
 
-def forward(port):
-    import requests
-    import json
 
-    ngrok = subprocess.Popen(['ngrok','http',str(port)],stdout=subprocess.PIPE)
+def forward(port):
+    try:
+        import requests
+        import json
+    except:
+        process_display(1,1,"Module not found\n    Make sure you have installed 'requirements.txt' and configured DASH")
+        sys.exit(0)
+
+    ngrok = subprocess.Popen(['ngrok','http','-region','ap' ,str(port)],stdout=subprocess.PIPE)
     process_display(1,0,"[1/2] Public Server is started.")
     time.sleep(3)
     tunnel_url = requests.get("http://localhost:4040/api/tunnels").text
@@ -59,8 +70,9 @@ def forward(port):
         j = json.loads(tunnel_url)
         tunnel_url = j['tunnels'][0]['public_url']
         process_display(0,0,"The link for the page is : "+tunnel_url)
-
     print("\n{}-------------------------------{} _^_ {}-------------------------------{}\n".format(colors.white,colors.red,colors.white,colors.red))
+
+
 
 
 if __name__ == "__main__":
@@ -70,24 +82,30 @@ if __name__ == "__main__":
         header()  # Prints logo on console. P.S : I'm not a designer :/
 
         try:
-            import sys,argparse
+            import argparse
             parser = argparse.ArgumentParser()
         except:
-            process_display(1,1,"Moudle not found")
+            process_display(1,1,"Module not found\n    Make sure you have installed 'requirements.txt' and configured DASH")
+            sys.exit(0)
 
         parser.add_argument('-port',help="Port address",required = False,default = 5050)
         parser.add_argument('-path',help="Path to save file",required = False,default=os.path.expanduser('~')+'/Desktop')
         parser.add_argument('-v',help="Verbose",default=False, action='store_true',required = False)
 
         args = parser.parse_args()
+        try:
+            import threading
+        except:
+            process_display(1,1,"Module not found\n    Make sure you have installed 'requirements.txt' and configured DASH")
+            sys.exit(0)
 
-        import threading
-
-        thread1 = threading.Thread(target=local,args=(args.port,args.path))
-        thread2 = threading.Thread(target=forward,args=(args.port,))
+        thread1 = threading.Thread(target=forward,args=(args.port,))
+        thread2 = threading.Thread(target=local,args=(args.port,args.path))
 
         thread1.start()
+        time.sleep(1)
         thread2.start()
+
 
         thread1.join()
         thread2.join()
